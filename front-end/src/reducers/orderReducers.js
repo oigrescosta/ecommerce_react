1b6e3b018b246630ae783ee6d3f623d59ad195f6
unknown <costa.sergio.86@gmail.com>
2021-01-01 21:39:28 +0000

    User details & orders reset

--- front-end/src/reducers/orderReducers.js
+++ front-end/src/reducers/orderReducers.js
@@ -9,6 +9,10 @@ import {
     ORDER_PAY_SUCCESS,
     ORDER_PAY_FAIL,
     ORDER_PAY_RESET,
+    ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_FAIL,
+    ORDER_LIST_MY_RESET,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -33,7 +37,7 @@ export const orderCreateReducer = (state = {}, action) => {
     }
 }
 
-export const orderDetailsReducer = (state = { loading:true, loadinorderItems: [], shippingAddress: {} }, action) => {
+export const orderDetailsReducer = (state = { loading:true, orderItems: [], shippingAddress: {} }, action) => {
     switch(action.type) {
         case ORDER_DETAILS_REQUEST:
             return {
@@ -59,7 +63,7 @@ export const orderPayReducer = (state = { }, action) => {
     switch(action.type) {
         case ORDER_PAY_REQUEST:
             return {
-                loading: true
+                loading: true,
             }
         case ORDER_PAY_SUCCESS:
             return {
@@ -73,6 +77,31 @@ export const orderPayReducer = (state = { }, action) => {
             }
         case ORDER_PAY_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const orderListMyReducer = (state = { orders: [] }, action) => {
+    switch(action.type) {
+        case ORDER_LIST_MY_REQUEST:
+            return {
+                loading: true,
+            }
+        case ORDER_LIST_MY_SUCCESS:
+            return {
+                loading: false,
+                orders: action.payload,
+            }
+        case ORDER_LIST_MY_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        case ORDER_LIST_MY_RESET:
+            return {
+                orders: []
+            }
         default:
             return state
     }
